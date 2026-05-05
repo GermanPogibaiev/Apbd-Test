d7c5ff24c9f9b373e3973d00eca183fa1c073e7b
German Pogibaiev <S32820@pjwstk.edu.pl>
2026-05-05 13:18:10 +0200

    Package + Program.cs Update

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using ApbdTest.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IDbService, DbService>();
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapControllers();
+
+app.Run();
